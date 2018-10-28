658544c3c784107a8344b661e6b4e490ae7589aa
Minusi <minusie357@gmail.com>
2018-10-28 22:12:01 +0900

    CREATE _ 언리얼 프로젝트의 생성
    # 세부사항
    1. 언리얼 프로젝트 생성
      - 프로젝트명 : FujiwaraBattleField
      - C++ / 기본 / 시작 콘텐츠 포함
    
    2. 콘텐츠 초기환경 구축
      - Maps 폴더 추가
      - Maps_LostBambooForest 애셋 추가 및 기초 라이팅 배치

---
+++ FujiwaraBattleField/Source/FujiwaraBattleField/FujiwaraBattleFieldGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FujiwaraBattleFieldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUJIWARABATTLEFIELD_API AFujiwaraBattleFieldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
