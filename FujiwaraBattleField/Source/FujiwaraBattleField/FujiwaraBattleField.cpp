bdededdf7240a8d16edcda650e1219d61a3df9dd
Minusi <minusie357@gmail.com>
2018-10-31 10:27:33 +0900

    SETTING _ C++ 유저 프레임워크 빌드 및 프로젝트 초기 로드맵 설정
    # 세부사항
    1. 유저 프레임워크 빌드
      - 프로젝트를 빌드하지 않아 사용자 dll이 생성되지 않은 이유로 실행 불가로 추측되는 프로젝트에 대한 C++ 빌드
    
    2. 초기 로드맵 설정
      - 시작시 Minimal_Default.umap에 진입하는 것을 프로젝트 설정에서 Maps_LostBambooForest.umap으로 변경

--- FujiwaraBattleField/Source/FujiwaraBattleField/FujiwaraBattleField.cpp
+++ FujiwaraBattleField/Source/FujiwaraBattleField/FujiwaraBattleField.cpp
@@ -1,6 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FujiwaraBattleField.h"
 #include "Modules/ModuleManager.h"
+#include "GameFramework/MovementComponent.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FujiwaraBattleField, "FujiwaraBattleField" );
