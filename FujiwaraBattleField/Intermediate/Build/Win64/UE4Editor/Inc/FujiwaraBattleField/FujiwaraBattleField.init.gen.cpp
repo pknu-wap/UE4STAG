3d5f9d9ba4a01ae162b5fa8145a69a7c39015fa6
Unknown <hun2ya9@naver.com>
2018-11-24 18:00:52 +0900

    BGM _ 배경 음악 시퀀서 제작
    #세부내용
    1. 배경 음악 시퀀서
     - 기본은 여러 음악을 선형적으로 실행
     - 선형만o 루프만o 선형으로 가다가 랜덤o 루프o
     - 문제점 : 언리얼 버그인지 wav 파일이 안되는 것이 존재 -> 이 시스템 못쓰게 될 수 있음

--- FujiwaraBattleField/Intermediate/Build/Win64/UE4Editor/Inc/FujiwaraBattleField/FujiwaraBattleField.init.gen.cpp
+++ FujiwaraBattleField/Intermediate/Build/Win64/UE4Editor/Inc/FujiwaraBattleField/FujiwaraBattleField.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeFujiwaraBattleField_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/FujiwaraBattleField",
 				PKG_CompiledIn | 0x00000000,
-				0xA011F82D,
-				0xB24B42E0,
+				0x50DB0EC5,
+				0x2EB01707,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
